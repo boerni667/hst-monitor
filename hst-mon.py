c53ffbf22a14e9924743778384e06bdcc806fb70
bhering <bernhard.hering@slub-dresden.de>
2023-03-18 01:18:11 +0100

    update 2023

--- hst-mon.py
+++ hst-mon.py
@@ -1,22 +1,22 @@
-#!/usr/bin/python3.5
+#!/usr/bin/python3
 
 import dvb
 import sys
 import signal
 import shutil
-import os
 import argparse
-import json
 from subprocess import call
+from datetime import datetime, timedelta
 from time import sleep
 
 
-station=None
-stations={
-"arbeit":  ["SLUB","TU Dresden","Zellescher Weg"],
-"zuhause": ["Cottaer Straße","Rosenstraße","Hp. Freiberger Straße","Tharandter Straße"],
-"hbf":     ["Hauptbahnhof","Hauptbahnhof Nord"]
+sets = {
+        "zuhause": {"Cottaer Straße":3,
+        "Hp. Freiberger Straße":10,
+        "Tharandter Straße":10}
 }
+
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -27,6 +27,8 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+
 ### don't mess up terminal by SIGINT or CTRL^C
 def signal_handler(signal,frame):
     call(["tput","cnorm"])
@@ -38,33 +40,35 @@ if __name__ == "__main__":
     parser=argparse.ArgumentParser(description='Fancy DVB Timetable Monitor')
     parser.add_argument('-set',type=str,default="zuhause",help='set of Stations to use')
     parser.add_argument('-help',action="store_true",help="print this help")
-    args=parser.parse_args()
-    if args.set:
-        if args.set in stations:
-            station=args.set
-        else:
-            sys.stderr.write("Selected set not in defined sets! Add your set to the sourcecode or choose from existing:"+bcolors.WARNING)
-            for k,v in stations.items():
-                sys.stderr.write(" "+str(k))
-            sys.stderr.write("\n")
-            exit(-1)
-    args=parser.parse_args()
+    args = parser.parse_args()
     if args.help:
         parser.print_help(sys.stderr)
         exit()
     signal.signal(signal.SIGINT,signal_handler)
     call(["tput","civis"])
+
     while True:
-        lines=shutil.get_terminal_size((85,25)).lines
-        if lines%2==0:
-            lines-=1
-        num=int(lines/len(stations[station]))
+        lines = shutil.get_terminal_size((85,25)).lines
+        if lines%2 == 0:
+            lines -= 1
+        num = int(lines/len(sets[args.set]))
         sys.stdout.write("\x1B[H\x1B[J")
-        sys.stdout.write(bcolors.OKGREEN+"{:<30s}{:<13s}{:<30s}{:>7s}".format("Haltestelle","Linie","Richtung","Ankunft")+bcolors.ENDC)
-        table=[]
-        for street in stations[station]:
+        sys.stdout.write(bcolors.OKGREEN+"{:<30s}{:<6s}{:<37s}{:>7s}{:>10s}".format("Haltestelle","Linie","Richtung","in","um")+bcolors.ENDC)
+        table = []
+        for street in sets[args.set]:
             sys.stdout.write("\n")
-            for elem in dvb.monitor(street,1,num-1,"Dresden"):
-                sys.stdout.write("\n{:<30s}".format(street)+bcolors.YELLOW+"{:<13s}".format(elem["line"])+bcolors.ENDC+"{:<30s}".format(elem["direction"])+"{:>7d}".format(elem["arrival"]))
-        sleep(10)
-        
+            monitor = {}
+            for elem in dvb.monitor(street,sets[args.set][street],num-1):
+                if elem["direction"] not in monitor:
+                    monitor[elem["direction"]] = []
+                monitor[elem["direction"]].append(elem)
+            for direction in monitor:
+                now = datetime.now()
+                for elem in monitor[direction]:
+                    sys.stdout.write("\n{:<30s}".format(street))
+                    sys.stdout.write(bcolors.YELLOW+"{:<6s}".format(elem["line"]))
+                    sys.stdout.write(bcolors.ENDC+"{:<37s}".format(elem["direction"]))
+                    sys.stdout.write("{:>7d} min".format(elem["arrival"]))
+                    sys.stdout.write("{:>10s}".format((now+timedelta(minutes=elem["arrival"])).strftime("%H:%M")))
+                    sys.stdout.flush()
+        sleep(30)
